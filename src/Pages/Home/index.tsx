27b7cf3a19833a4bb37b34d3f7c5b9a7f7d0cb97
Gilvan Izidio <gilvam.izidio.dev@gmail.com>
2022-03-22 00:30:28 -0300

    feat: iniciado aplicação e adicionado icone

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Home = (): JSX.Element => {
+  return (
+    <Container>
+      <></>
+    </Container>
+  );
+};
+
+export default Home;
